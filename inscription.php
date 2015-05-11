8904ce48d000ec8c8b9d60cdd0f852b61c72620d
CamilleGorce <camille.gorce@isep.fr>
2015-05-11 18:04:49 +0200

    Essai
    Essai

--- inscription.php
+++ inscription.php
@@ -1,28 +1,46 @@
-<?php
+<?php 
+include_once('Class/Membre.php');
 if($_POST){
-try
-{
-//il essaie de se connecter à la base
-$bdd = new PDO('mysql:host=localhost;dbname=trocandcroc;charset=utf8', 'root', '');
+$verif = true;
+if(empty($_POST["pseudo"])){
+$verif = false;
+echo "Pas de pseudo<br/>";
 }
-// si il n'arrive pas il recupere l'exeption et l'afiche en message d'erreur
-catch(Exception $e)
-{
-die('Erreur : '.$e->getMessage());
+if(empty($_POST["nom"])){
+$verif = false;
+echo "Pas de nom</br>";
 }
-/*
-if(!empty($_POST["nom"]))
-echo $_POST["nom"];
-else
-echo "Pas de nom";
-*/
-// crypter en md5 un mdp
+if(empty($_POST["prenom"])){
+$verif = false;
+echo "Pas de prénom</br>";
+}
+if(empty($_POST["adresse"])){
+$verif = false;
+echo "Pas d'adresse</br>";
+}
+if(empty($_POST["cp"])){
+$verif = false;
+echo "Pas de code postal</br>";
+}
+if(empty($_POST["ville"])){
+$verif = false;
+echo "Pas de ville</br>";
+}
+if(empty($_POST["telephone"])){
+$verif = false;
+echo "Pas de téléphone</br>";
+}
+if(empty($_POST["mail"])){
+$verif = false;
+echo "Pas de mail</br>";
+}
+if(empty($_POST["sexe"])){
+$verif = false;
+echo "Pas de sexe</br>";
+}
+if($verif){
 $pass_md5 = md5($_POST["motdepasse"]);
-// Insertion dans la base de donnée
-//On prepare la requete INSERT INTO
-$req = $bdd->prepare('INSERT INTO membre(pseudo, motdepasse, nom, prenom, adresse, cp, ville, telephone, mail, sexe, admin) VALUES(:pseudo, :motdepasse, :nom, :prenom, :adresse, :cp, :ville, :telephone, :mail, :sexe, :admin)');
-// On execute la requete avec les variables correspondantes
-$req->execute(array(
+$new_user = array(
 'pseudo' => $_POST["pseudo"],
 'motdepasse' => $pass_md5,
 'nom' => $_POST["nom"],
@@ -34,106 +52,77 @@ $req->execute(array(
 'mail' => $_POST["mail"],
 'sexe' => $_POST["sexe"],
 'admin' => 0
-)
 );
+$resultat = Membre::sinscrire($new_user);
+if(!$resultat)
+echo "Votre pseudo existe déjà !";
+else
+echo "Vous êtes bien inscrit !";
+}
 }
 ?>
 <DOCTYPE html>
 <html>
 <head>
-	<title>INSCRIPTION</title>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="styleinscription.css" />
+<title>INSCRIPTION</title>
+<meta charset="UTF-8">
 </head>
 <body>
-	<h1> INSCRIPTION </h1>
-</br>
-	<div class="Titres">
-	 	<p>Informations de connection : </p> 
-	</div>
-	<form action="" method="post">
-		<table>
-			<tr class="tr_hauteur">
-				<td class="td_largeur"><label for="pseudo">Pseudo :</label></td>
-				<td class="td_largeur"><input type="text" name="pseudo" id="pseudo"/></td>
-			</tr>
-
-			<tr class="tr_hauteur">
-				<td class="td_largeur"><label for="motdepasse">Mot de passe :</label></td>
-				<td class="td_largeur"><input type="password" name="motdepass" id="motdepasse"/></td>
-			</tr>
-
-			<tr class="tr_hauteur">
-				<td class="td_largeur"><label for="pass2">Confirmez le mot de passe :</label></td>
-				<td class="td_largeur"><input type="password" name="pass2" id="pass2"/></td>
-			</tr>
-		</table>
-	</br>
-	</br>
-		<div class="Titres">
-			 	<p>Informations personnelles : </p> 
-		</div>
-		<table>
-			<tr class="tr_hauteur">
-				<td class="td_largeur"><label for="sexe">Sexe :</label></td>
-				<td class="td_largeur">
-					<input type= "radio" name="sexe" value="H"> Femme
-					<input type= "radio" name="sexe" value="F"> Homme
-				</td>
-			</tr>
-
-			<tr class="tr_hauteur">
-				<td class="td_largeur"><label for="nom">Nom :</label></td>
-				<td class="td_largeur"><input type="text" name="nom" id="nom"/></td>
-			</tr>
-
-			<tr class="tr_hauteur">
-				<td class="td_largeur"><label for="prenom">Prénom :</label></td>
-				<td class="td_largeur"><input type="text" name="prenom" id="prenom"/></td>
-			</tr>
-
-			<tr class="tr_hauteur">
-				<td class="td_largeur"><label for="mail">E-mail :</label></td>
-				<td class="td_largeur"><input type="email" name="mail" id="mail" placeholder="troc&croc@gmail.com"/></td>
-			</tr>
-
-			<tr class="tr_hauteur">
-				<td class="td_largeur"><label for="adresse">Adresse :</label></td>
-				<td class="td_largeur"><input type="text" name="adresse" id="adresse"/></td>
-			</tr>
-
-			<tr class="tr_hauteur">
-				<td class="td_largeur"><label for="cp">Code postal :</label></td>
-				<td class="td_largeur"><input type="text" name="cp" id="cp"/></td>
-			</tr>
-
-			<tr class="tr_hauteur">
-				<td class="td_largeur"><label for="ville">Ville :</label></td>
-				<td class="td_largeur"><input type="text" name="ville" id="ville"/></td>
-			</tr>
-
-			<tr class="tr_hauteur">
-				<td class="td_largeur"><label for="telephone">Téléphone :</label></td>
-				<td class="td_largeur"><input type="tel" name="telephone" id="telephone"/></td>
-			</tr>
-
-			<tr class="tr_hauteur">
-				<td class="td_largeur"><label for="photo">Photo :</label></td>
-				<td class="td_largeur"><input type="file" name="photo" id="photo"/></td>
-			</tr>
-		</table>
-			</br>
-			<p class="Jaccepte"> <input type="checkbox"/> J'ai lu et j'accepte les <a href="Conditions.html">conditions d'utilisation</a></p>
-			
-			<div class="Fin">
-				<input type="submit" name="register" value="S'inscrire"/>
-			</br>
-			</br>
-			</br>
-				<a href="index.php">Retour</a>
-			</br>
-			</br>
-			</div>
-				
-	</form>
+<form action="" method="post">
+<table>
+<tr>
+<td><label for="pseudo"><strong>Pseudo :</strong></label></td>
+<td><input type="text" name="pseudo" id="pseudo"/></td>
+</tr>
+<tr>
+<td><label for="motdepasse"><strong>Mot de passe :</strong></label></td>
+<td><input type="password" name="motdepasse" id="motdepasse"/></td>
+</tr>
+<tr>
+<td><label for="pass2"><strong>Confirmez le mot de passe :</strong></label></td>
+<td><input type="password" name="pass2" id="pass2"/></td>
+</tr>
+<tr>
+<td><label for="nom"><strong>Nom :</strong></label></td>
+<td><input type="text" name="nom" id="nom"/></td>
+</tr>
+<tr>
+<td><label for="prenom"><strong>Prénom :</strong></label></td>
+<td><input type="text" name="prenom" id="prenom"/></td>
+</tr>
+<tr>
+<td><label for="adresse"><strong>Adresse :</strong></label></td>
+<td><input type="text" name="adresse" id="adresse"/></td>
+</tr>
+<tr>
+<td><label for="cp"><strong>Code postal :</strong></label></td>
+<td><input type="text" name="cp" id="cp"/></td>
+</tr>
+<tr>
+<td><label for="ville"><strong>Ville :</strong></label></td>
+<td><input type="text" name="ville" id="ville"/></td>
+</tr>
+<tr>
+<td><label for="telephone"><strong>Téléphone :</strong></label></td>
+<td><input type="text" name="telephone" id="telephone"/></td>
+</tr>
+<tr>
+<td><label for="photo"><strong>Photo :</strong></label></td>
+<td><input type="file" name="photo" id="photo"/></td>
+</tr>
+<tr>
+<td><label for="mail"><strong>E-mail :</strong></label></td>
+<td><input type="text" name="mail" id="mail"/></td>
+</tr>
+<tr>
+<td><label for="sexe"><strong>Sexe :</strong></label></td>
+<td>
+<input type= "radio" name="sexe" value="H"> Homme
+<input type= "radio" name="sexe" value="F"> Femme
+</td>
+</tr>
+</table>
+<input type="submit" name="register" value="S'inscrire"/>
+</form>
+<a href="index.php">Retour</a>
 </body>
